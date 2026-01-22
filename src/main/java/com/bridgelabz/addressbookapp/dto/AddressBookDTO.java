c0830f57b07633d0b9d6543c7a0c0ebcba096dad
BL-Stalin <stalin.gunasekaran@bridgelabz.com>
2026-01-22 12:32:56 +0530

    Day6 AddressBook Section3 UC1: added Lombok to DTO and Model

--- src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDTO.java
+++ src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDTO.java
@@ -1,28 +1,10 @@
 package com.bridgelabz.addressbookapp.dto;
 
-public class AddressBookDTO {
+import lombok.Data;
 
+@Data
+public class AddressBookDTO {
     private String name;
     private String phoneNumber;
     private String city;
-
-    public AddressBookDTO() {}
-
-    public AddressBookDTO(String name, String phoneNumber, String city) {
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-        this.city = city;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public String getCity() {
-        return city;
-    }
 }
